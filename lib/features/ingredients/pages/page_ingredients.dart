bf2d3dc9b47286bd78d46939e32979b0448d447e
Fikri Razzaq <fikrirazzaq30@gmail.com>
2020-07-19 21:17:47 +0700

    fix: handle clear current selected ingredients.

--- lib/features/ingredients/pages/page_ingredients.dart
+++ lib/features/ingredients/pages/page_ingredients.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tech_task/features/ingredients/ingredients.dart';
 
 import '../../../widgets/widgets.dart';
 
-class PageIngredients extends StatelessWidget {
+class PageIngredients extends StatefulWidget {
   final DateTime selectedDate;
 
   const PageIngredients({Key key, this.selectedDate}) : super(key: key);
 
   @override
+  _PageIngredientsState createState() => _PageIngredientsState();
+}
+
+class _PageIngredientsState extends State<PageIngredients> {
+  CurrentIngredientsBloc currentIngredientsBloc;
+  @override
+  void initState() {
+    super.initState();
+    currentIngredientsBloc = BlocProvider.of<CurrentIngredientsBloc>(context);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return TmpIngredients(selectedDate: selectedDate);
+    return TmpIngredients(
+      selectedDate: widget.selectedDate,
+      onBackPressed: () async {
+        currentIngredientsBloc.add(ClearIngredientsEvent());
+        Navigator.pop(context);
+        return true;
+      },
+    );
   }
 }
--- lib/widgets/templates/tmp_ingredients.dart
+++ lib/widgets/templates/tmp_ingredients.dart
@@ -1,19 +1,41 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import '../widgets.dart';
 
-class TmpIngredients extends StatelessWidget {
+class TmpIngredients extends StatefulWidget {
   final DateTime selectedDate;
+  final Function onBackPressed;
 
-  const TmpIngredients({Key key, this.selectedDate}) : super(key: key);
+  const TmpIngredients({Key key, this.selectedDate, this.onBackPressed})
+      : super(key: key);
+
+  @override
+  _TmpIngredientsState createState() => _TmpIngredientsState();
+}
+
+class _TmpIngredientsState extends State<TmpIngredients> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AtmAppbarPrimary(context, text: 'Recipe Suggestion'),
-      body: OrgIngredients(
-        selectedDate: selectedDate,
+    return WillPopScope(
+      onWillPop: widget.onBackPressed,
+      child: Scaffold(
+        appBar: AtmAppbarPrimary(
+          context,
+          text: 'Recipe Suggestion',
+          leading: IconButton(
+            icon: Platform.isIOS
+                ? Icon(Icons.arrow_back_ios)
+                : Icon(Icons.arrow_back),
+            onPressed: widget.onBackPressed,
+          ),
+        ),
+        body: OrgIngredients(
+          selectedDate: widget.selectedDate,
+        ),
+        floatingActionButton: MolFabShowRecipe(),
       ),
-      floatingActionButton: MolFabShowRecipe(),
     );
   }
 }
